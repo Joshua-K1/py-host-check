db3f85eb4e3a3ff7568e17db8ab1d21d4cc5c32f
Joshua-K1 <leenux@joshua-lab-vm.sqw41ai2pr1ehgqjsdj5llhw5b.fx.internal.cloudapp.net>
2023-08-01 22:36:17 +0000

    Printing Services

--- ub_host_check.py
+++ ub_host_check.py
@@ -42,7 +42,7 @@ def check_service_versions():
     print("Checking Service Versions...")
 
     for s_name in service_list:
-        print("Service name is: " + s_name)
+        #print("Service name is: " + s_name)
         for line in os.popen(f"systemctl status {s_name}"):
             if line.strip():
                 # include with flag for debug output?
@@ -53,31 +53,35 @@ def check_service_versions():
                 if "Active" in line:
                     s_active = line.split()
                     s_active = s_active[1]
-                    print(s_active)
 
                 # if line contains memory, print it
                 if "Memory" in line  and "MemoryAccounting" not in line:
                     s_memory = line.split()
                     s_memory = s_memory[1]            
-                    print(s_memory)
 
                 if "Loaded" in line:
                     s_loaded = line.split()
                     s_loaded = s_loaded[1]
-                    print(s_loaded)
 
                 # if line contains main, print it
                 if "Main" in line:
                     s_pid = line.split()
                     s_pid = s_pid[2]
-                    # print(s_pid)
 
                 # if line contains CPU, print it
-                if "CPU" in line:
+                if "CPU" in line and "CPUAccounting" not in line:
                     s_cpu = line.split()
                     s_cpu = s_cpu[1]
-                    # print(s_cpu)
 
         
         service_meta = Services(s_name, s_loaded, s_active, s_pid, s_memory, s_cpu)
         service_library.append(service_meta)
+        
+
+    for service_a in service_library:
+        print("="*5, service_a.name,"="*5)
+        print("Loaded: ", service_a.loaded)
+        print("Active: ", service_a.active)
+        print("Memory: ", service_a.memory)
+        print("CPU:    ", service_a.cpu)
+        print("PID:    ", service_a.pid)
